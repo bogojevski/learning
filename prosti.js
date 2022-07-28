437526ef4f1ae3c0acf8fa09dab8437de47fe3f7
bogojevski <32037058+bogojevski@users.noreply.github.com>
2022-07-28 18:18:15 +0200

    Update prosti.js

--- prosti.js
+++ prosti.js
@@ -1,7 +1,11 @@
 const print = console.log
-const x = 3, y = 4, z = 101
+const x = -3, y = 4, z = 101
 
 function prost(n) {
+    if (n < 2) {
+        throw "Brojot " + n + " ne smee da bide pomal od 2"
+    }
+
     i = 2
     while (n > i) {
         if (n % i == 0) {
@@ -11,6 +15,7 @@ function prost(n) {
             i++
         }
     }
+
     if (n == i) {
         print(n + " e prost")
     }
